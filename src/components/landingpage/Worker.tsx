3f8d4aec43979dc9ca2a667f6ebf4cbe57d24f0a
Lim Kit Siang <127509175+JihadZaidan@users.noreply.github.com>
2025-09-03 14:43:33 +0700

    repaired folder

--- src/components/landingpage/Worker.tsx
+++ src/components/landingpage/Worker.tsx
@@ -2,14 +2,14 @@ import Image from "next/image";
 import { ArrowRight } from "lucide-react";
 import cloody from "../../../public/landingpage/worker/cloody.png"
 import dockey from "../../../public/landingpage/worker/docker.png"
-import house from "../../../public/landingpage/worker/housels.png"
-import ai from "../../../public/landingpage/worker/intelegence.png"
+import house from "../../../public/landingpage/worker/housel.png"
+import ai from "../../../public/landingpage/worker/contai.png"
 
 export default function WorkerFiles() {
     return (
         <div className="max-w-full w-full bg-neutral-100 lg:px-20 lg:pt-27 lg:pb-30 px-8 pt-10 pb-14">
             <div className="w-full flex flex-col lg:gap-[-150px] gap-8">
-                <h2 className="text-right text-neutral-800 lg:text-6xl text-3xl leading-[140%]">The work we do, <br /> and the people we help</h2>
+                <h2 className="text-right text-neutral-800 lg:text-8xl text-3xl leading-[140%]">The work we do, <br /> and the people we help</h2>
                 <div className="w-full flex lg:flex-row flex-col lg:gap-25">
                     <div className="w-full flex flex-col lg:gap-20 gap-10">
                         <div className="w-full flex flex-col gap-8">
