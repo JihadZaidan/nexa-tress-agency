b47cba369907388ed57b80158ad809eb614e167a
Lim Kit Siang <127509175+JihadZaidan@users.noreply.github.com>
2025-09-03 14:35:59 +0700

    repaired and update components importing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import Featurest from "@/components/landingpage/FeatureLand";
 import Resprime from "@/components/landingpage/HeadPrimary";
-import Image from "next/image";
+import WorkerFiles from "@/components/landingpage/Worker";
 
 export default function Home() {
   return (
     <div className="w-full">
       <Resprime/>
       <Featurest/>
+      <WorkerFiles/>
     </div>
   );
 }
