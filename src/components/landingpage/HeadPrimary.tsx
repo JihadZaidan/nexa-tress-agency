cdcb3e3e27a4b7844f2f9b893ff79f9090a46e38
Lim Kit Siang <127509175+JihadZaidan@users.noreply.github.com>
2025-09-05 11:15:54 +0700

    feat(gsap): implement effect

--- src/components/landingpage/HeadPrimary.tsx
+++ src/components/landingpage/HeadPrimary.tsx
@@ -1,15 +1,27 @@
+"use client"
+
 import Image from "next/image"
 import team from "../../../public/landingpage/teams.png"
 import { Button } from "../ui/button"
 import { ArrowRight } from "lucide-react"
+import { useSlideFromTop, useSlideFromLeft, useSlideFromRight } from "@/library/animations"
+import { useRef } from "react"
 
 export default function Resprime() {
+    const headRef = useRef<HTMLHeadingElement>(null);
+    const timsRef = useRef<HTMLDivElement>(null);
+    const paraRef = useRef<HTMLDivElement>(null);
+
+    useSlideFromTop(headRef, 0.3);
+    useSlideFromRight(timsRef, 0.3);
+    useSlideFromLeft(paraRef, 0.3);
+
     return (
         <div className="max-w-full w-full lg:px-20 lg:py-16 px-5 pt-7 pb-14 bg-neutral-100">
             <div className="w-full flex flex-col justify-between lg:gap-30 gap-8">
                 <div className="w-full flex flex-col justify-right lg:gap-15 gap-8">
-                    <h1 className="w-full text-neutral-800 font-sans font-medium lg:text-8xl text-5xl lg:text-center text-left leading-[130%]">NEXA DIGITAL AGENCY</h1>
-                    <div className="lg:ml-130 lg:w-[50%] flex lg:flex-row flex-col lg:gap-5 gap-3 justify-left items-left">
+                    <h1 ref={headRef} className="w-full text-neutral-800 font-sans font-medium lg:text-8xl text-5xl lg:text-center text-left leading-[130%]">NEXA DIGITAL AGENCY</h1>
+                    <div ref={timsRef} className="lg:ml-130 lg:w-[50%] flex lg:flex-row flex-col lg:gap-5 gap-3 justify-left items-left">
                         <Image
                             src={team}
                             alt="photos"
@@ -20,7 +32,7 @@ export default function Resprime() {
                 </div>
 
                 <div className="w-full flex lg:flex-row items-left justify-left">
-                    <div className="flex flex-col gap-8">
+                    <div ref={paraRef} className="flex flex-col gap-8">
                         <p className="font-sans font-medium lg:text-2xl text-base text-neutral-800 leading-[150%] text-left">
                             — An digital agency focused on digital <br />
                             experiences. With every single one of <br />
