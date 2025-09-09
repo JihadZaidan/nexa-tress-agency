15c2afffd730480a4296412271a5e72e028c0f13
Lim Kit Siang <127509175+JihadZaidan@users.noreply.github.com>
2025-09-09 09:01:12 +0700

    Add files via upload

---
+++ src/app/works/page.tsx
@@ -0,0 +1,11 @@
+import Worked from "@/components/works/header";
+import Jobdesk from "@/components/works/workjob";
+
+export default function Works() {
+    return (
+        <div className="w-full">
+            <Worked/>
+            <Jobdesk/>
+        </div>
+    )
+}
